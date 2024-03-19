d2fc3f1ee1f43a6c2fee6095cb770774dd30a69e
Lars-Ragnar A. Haugen <lars.ragnar.haugen@origo.oslo.kommune.no>
2024-03-19 20:13:52 +0100

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# my-punkt-test
+
+```
+npm create vue@latest
+npm i @oslokommune/punkt-assets @oslokommune/punkt-css @oslokommune/punkt-vue
+```
+
+- Lagt til PktBacklink i App.vue
+- Fjernet bloat fra `npm create vue`
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { PktBackLink } from "@oslokommune/punkt-vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+
+  <main>
+    <PktBackLink href="http://localhost:5173" text="Gå tilbake" />
+    <section>Masse bra innhold.</section>
+  </main>
+</template>
+<style scoped>
+@import "@pkt/css/pkt.css";
+</style>
